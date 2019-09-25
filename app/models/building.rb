83bc53e3c3aa69862a3e1451a7afcb7a535c1608
“Cherylshire” <hsuanchang2017@gmail.com>
2019-09-25 16:09:35 -0500

    update

---
+++ app/models/building.rb
@@ -0,0 +1,29 @@
+class Building
+  attr_reader :id, :name, :address, :height, :construction_date, :architect
+  def initialize(input_options)
+    @id = input_options["id"]
+    @name = input_options["name"]
+    @address = input_options["address"]
+    @height = input_options["height"]
+    @construction_date = input_options["construction_date"]
+    @architect = input_options["architect"]
+  end
+
+  def self.find(input_id) # for show
+    response = HTTP.get("http://localhost:3000/api/buildings/#{input_id}")
+    hash_data = response.parse
+    Building.new(hash_data)
+  end
+
+  def self.all # for index
+    response = HTTP.get("http://localhost:3000/api/buildings")
+    array_of_hashes = response.parse
+    array_of_hashes.map do |hash|
+      Building.new(hash)
+    end 
+  end
+  def self.create # for create
+    response = HTTP.post("http://localhost:3000/api/buildings")
+    response.parse
+  end
+end
